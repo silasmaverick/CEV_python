f3045f43128f7534cac2c31408d71e243ddbd385
Silas Lino <silasmaverick@gmail.com>
2021-07-09 15:12:03 -0300

    Atualização com o tipo do triângulo

--- retas_triangulo.py
+++ retas_triangulo.py
@@ -1,7 +1,17 @@
-a = float(input('Digite a reta A: '))
-b = float(input('Digite a reta B: '))
-c = float(input('Digite a reta C: '))
+a = float(input('Digite o comprimento da reta A: '))
+b = float(input('Digite o comprimento da reta B: '))
+c = float(input('Digite o comprimento da reta C: '))
+tipo = 'a'
+
+if a == b and a == c:
+    tipo = 'Equilátero'
+elif a == b and a != c or a == c and a != b or b == c and b != a:
+    tipo = 'Isósceles'
+else:
+    tipo = 'Escaleno'
+
 if (b - c) < a < (b + c) and (a - c) < b < (a + c) and (a - b) < c < (a + b):
-    print('Pode-se formar um triângulo')
+    print(f'Pode-se formar um triângulo {tipo}')
+
 else:
     print ('Não pode se formar um triângulo')
