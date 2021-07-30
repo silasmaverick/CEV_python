d4dd19a42829ddf461e3601595992bf47c567ad5
Silas Lino <silasmaverick@gmail.com>
2021-07-30 16:17:49 -0300

    Atualização do jogo com o loop while

--- jogo_da_memoria.py
+++ jogo_da_memoria.py
@@ -1,19 +1,41 @@
+#%%
 ##exercicio 28 jogo de adivinhação
 from random import randint
 from time import sleep
 print('=-'*30)
-print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('Vou pensar em um número entre 1 e 7. Tente adivinhar...')
 print('=-'*30)
-pc = randint(0, 5)
+pc = randint(1, 7)
 print('Pensando.....')
 sleep(2)
 print('Já pensei!!')
 print('=-'*30)
 n = int(input('Em que número eu pensei? '))
 print('.'*5)
-sleep(3)
+sleep(2)
+cont = 1
+
 if n == pc:
     print('Você ADIVINHOU!!!')
-else:
-    print('Você errou!! Não foi dessa vez!!! =(')
     print(f'O número correto é: {pc}')
+    print(f'Você precisou de {cont} tentativas')
+
+if n != pc:
+    while n != pc:               
+        print('Você errou!! Não foi dessa vez!!! =(')
+        t = int(input('Tente novamente... Digite um número: '))
+        n = t
+        cont +=1
+        print('.'*5)
+        sleep(1)
+        
+        
+
+if n == pc:
+    print('Você ADIVINHOU!!!')
+    print(f'O número correto é: {pc}')
+    print(f'Você precisou de {cont} tentativas')
+
+
+
+# %%
